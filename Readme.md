e669598ce837649bf48ce9dec0178503c91c9b4d
EtherCD <ethernetcoder@gmail.com>
2025-12-13 00:07:06 +0200

    Mouse impl

--- Readme.md
+++ Readme.md
@@ -3,3 +3,15 @@
   <h1>Altverse</h1>
   <p>A simple game/engine, a parody of evades.io</p>
 </div>
+
+# About
+
+This version is an implementation of rust-napi for performance.
+
+`A game similar to the popular evades.io.`
+
+**Currently in deep development and finalization**
+
+# Used
+
+**napi-rs, ajv, uwebsocket.js**
--- server/src/network/ws/handlers/mousepos.ts
+++ server/src/network/ws/handlers/mousepos.ts
@@ -3,5 +3,6 @@ import { Client } from "..";
 import { logger } from "../../../services/logger";
 
 export const mousePos = (ws: WebSocket<Client>, pos: [number, number]) => {
-	ws.getUserData().input.mousePos = [pos[0], pos[1]];
+  ws.getUserData().input.setMousePosX(pos[0]);
+  ws.getUserData().input.setMousePosY(pos[1]);
 };
